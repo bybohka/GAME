25b57d15d1489229522f7c460d7b0909e1f95d0e
savilokirill@gmail.com <savilokirill@gmail.com>
2025-10-07 01:00:23 +0300

    another new line

--- GuessNumber.java
+++ GuessNumber.java
@@ -53,6 +53,9 @@ public class GuessNumber {
                 System.out.println("Увы! Вы исчерпали все попытки. Загаданное число было: " + secretNumber);
             }
 
+
+            System.out.print("KFKFKFKF ");
+
             // Предложение сыграть еще раз
             System.out.print("Хотите сыграть еще раз? (да/нет): ");
             String playAgain = scanner.next();
